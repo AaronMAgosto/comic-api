914dc5a7892537c42c072fdeba361733b2025b87
Aaron M. Agosto <aaronagosto0613@gmail.com>
2023-06-29 12:47:26 -0400

    connected api to db

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,12 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+import functions from "firebase-functions"
+import express from "express"
+import cors from "cors"
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
+const app = express();
+app.use(express.json());
+app.use(cors());
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+
+
+export const api = functions.https.onRequest( app );
